848b246c22ccf7a7c7361cb9da7d5547c1ac7fb4
Fabio-Granella <168829097+Fabio-Granella@users.noreply.github.com>
2024-05-03 11:41:43 -0300

    Animação de Palavras

--- sketch.js
+++ sketch.js
@@ -3,8 +3,19 @@ function setup() {
 }
 
 function draw() {
-  background(220);
-  text("Professores", 138, 184);
-  fill(color("#F70B5A"));
+  background("white");
+  fill("black");
+  textSize(64);
+  textAlign(CENTER, CENTER);
+  
+  let maximo = width;
+  let minimo = 0;
+  // mouseX, 0, width ==> 0, palavra.length
+  let palavra = "Professores";
+  let quantidade = map(mouseX, 0, width, 1, palavra.length);
+  //console.log(quantidade);
+  let parcial = palavra.substring(0, quantidade);
+  text(parcial, 200, 200);
   
 }
+
